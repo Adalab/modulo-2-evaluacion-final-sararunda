9dc71a4c9664f67389f7de179f2d47ccb1d853af
Sara Fernández Ramos <sararunda@hotmail.com>
2022-04-07 16:52:25 +0200

    add fetch

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 'use strict';
-
-console.log('hola');
+//variables globales
+const listDrinks = document.querySelector('.js-list');
+const urlServer =
+  'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
+let drinks = [];
+//peticion al servidor
+fetch(urlServer)
+  .then((response) => response.json())
+  .then((data) => {
+    drinks = data.drinks;
+    console.log(drinks);
+  });
