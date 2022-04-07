cbc0c9b382cd1c66c5dd0ff849b1712e4298f634
Sara Fernández Ramos <sararunda@hotmail.com>
2022-04-07 18:33:32 +0200

    paint list coctels

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,32 @@
 'use strict';
 //variables globales
 const listDrinks = document.querySelector('.js-list');
-const urlServer =
-  'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
+const urlServer = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+const inputSearch = document.querySelector('.js-input');
+const btnSearch = document.querySelector('.js-btn-search');
+
 let drinks = [];
 //peticion al servidor
-fetch(urlServer)
-  .then((response) => response.json())
-  .then((data) => {
-    drinks = data.drinks;
-    console.log(drinks);
-  });
+
+//evento a escuchar
+
+function handleClick(event) {
+  event.preventDefault();
+  fetch(urlServer + inputSearch.value)
+    .then((response) => response.json())
+    .then((data) => {
+      drinks = data.drinks;
+      console.log(drinks);
+      //renderizar HTML
+      let html = '';
+      for (const drink of drinks) {
+        html += `<li>`;
+        html += `<h2>${drink.strDrink}</h2>`;
+        html += `<img class = "drinks-photo" src=${drink.strDrinkThumb} alt ="Foto bebida"/>`;
+        html += `</li>`;
+      }
+      listDrinks.innerHTML = html;
+    });
+}
+
+btnSearch.addEventListener('click', handleClick);
